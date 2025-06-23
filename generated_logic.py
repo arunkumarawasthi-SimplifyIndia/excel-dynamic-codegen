b24439cf1fa539807a90c31b8d4e742ef0d9a034
arunkumarawasthi-SimplifyIndia <arunkumarawasthi@gmail.com>
2025-06-23 21:04:27 +0530

    Add files via upload

--- generated_logic.py
+++ generated_logic.py
@@ -1,6 +1,16 @@
-def calculate(data):
-    data['#_processed'] = data['#']  # Placeholder logic
-    data['Payment_processed'] = data['Payment']  # Placeholder logic
-    data['Due Date_processed'] = data['Due Date']  # Placeholder logic
-    data['Unnamed: 3_processed'] = data['Unnamed: 3']  # Placeholder logic
+
+def calculate(data, computation_sheet):
+    data['D2_processed'] = computation_sheet.iloc[:, 1].iloc[0]  # from VLOOKUP
+    data['F2_processed'] = computation_sheet.iloc[:, 4].iloc[0]  # from VLOOKUP
+    data['D3_processed'] = computation_sheet.iloc[:, 6].iloc[0]  # from VLOOKUP
+    data['D4_processed'] = computation_sheet.iloc[:, 47].iloc[0]  # from VLOOKUP
+    data['F4_processed'] = computation_sheet.iloc[:, 6].iloc[0]  # from VLOOKUP
+    data['D5_processed'] = computation_sheet.iloc[:, 8].iloc[0]  # from VLOOKUP
+    data['B8_processed'] = computation_sheet.iloc[:, 20].iloc[0]  # from VLOOKUP
+    data['D8_processed'] = computation_sheet.iloc[:, 20].iloc[0]  # from VLOOKUP
+    data['F8_processed'] = data['D8'].astype(float) - data['B8'].astype(float)
+    data['B10_processed'] = computation_sheet.iloc[:, 20].iloc[0]  # from VLOOKUP
+    data['B12_processed'] = computation_sheet.iloc[:, 24].iloc[0]  # from VLOOKUP
+    data['D12_processed'] = computation_sheet.iloc[:, 28].iloc[0]  # from VLOOKUP
+    data['B18_processed'] = 'NumToWords logic to be implemented'
     return data
